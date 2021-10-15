5b492c4a15453f9e262e6ddc91e09b8dd1c1b6a0
aiorhiroki <1234defgsigeru@gmail.com>
2021-10-15 16:35:45 +0900

    ml flow

--- farmer/domain/model/trainer_model.py
+++ farmer/domain/model/trainer_model.py
@@ -39,6 +39,15 @@ class Trainer(Config, ImageLoader):
     trial_params: dict = None
     pruner: str = "MedianPruner"
     pruner_params: dict = None
+    sdice_tolerance: float = 0.0
+    mlflow: bool = False
+    experiment_name: str = None
+    run_name: str = None
+    description: str = None
+    tracking_uri: str = None
+    data_dvc_path: dict = None
+    version: str = None
+    user_name: str = None
 
     def __post_init__(self):
         self.task = self.get_task()
